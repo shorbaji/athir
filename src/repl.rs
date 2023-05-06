33fc8c9bd940f39d69618510626e7f6da2b909c6
Omar Shorbaji <github@shorbaji.com>
2023-05-06 19:41:16 -0400

    repl documentation

--- src/repl.rs
+++ src/repl.rs
@@ -7,38 +7,59 @@ use crate::eval::{eval, start};
 use crate::stdlib::base::{car, cons, read};
 use crate::value::{V};
 
+///
+/// repl()
+/// 
+/// Implements the read eval print loop by starting the interpreter driver loop (trampline) with
+/// the initial continuation to prompt the user for input and then read
+/// 
+/// The driver loop (trampoline) is implemented by the start() function in src/eval.rs
+/// It takes a continuation and an expression and calls the continuation with the expression
+/// The continuation returns a new continuation and a new expression to the trampoline
+/// 
+
 pub fn repl() {
     let env = global_env();
 
+    // Create the initial read continuation which 
+    let k = A::continuation(read_cont, &env, &A::continuation_null());
+
+    // Create a pair of ports for stdin and stdout
     let ports = cons(&A::port_stdin(), &A::port_stdout());
 
-    let k = A::continuation_null();
-    let k = A::continuation_plus(prompt_read_cont, &ports, &env, &k);
-
-    start(&k, &A::null());
+    // start the interpreter driver loop (trampoline) with the read continuation and the pair of ports
+    start(&k, &ports);
 }
 
-fn prompt_read_cont(e: &R, ports: &R, r: &R, k: &R) -> (R, R) {
-    if let V::String(s) = e.deref().borrow().deref() {
-        print!("{} ", s);
-        std::io::stdout().flush().unwrap();
-    }
+/// The read continuation function prompts the user for input and then reads the input
+/// It returns an eval continuation and the expression read
 
-    let k = A::continuation_plus(eval_cont, ports, r, k);
+fn read_cont(e: &R, r: &R, k: &R) -> (R, R) {
+    // Create an eval continuation
+    let k = A::continuation_plus(eval_cont, e, r, k);
 
-
+    // Prompt the user for input
     print!("> ");
     std::io::stdout().flush().unwrap();
 
-    let stdin_port = car(ports);
+    // e carries a pair of ports, stdin and stdout
+    // stdin is the first port in the pair
+    let stdin_port = car(e);
 
-
+    // Read the input from stdin and return the eval continuation and the expression read
     (k, read(Some(&stdin_port)))
 
 }
+
+/// The eval continuation function evaluates the expression read by the read continuation
+/// It returns a print continuation and the expression evaluated
 fn eval_cont(e: &R, ports: &R, r: &R, k: &R) -> (R, R) {
+
+    // check if the expression is an EOF object
+    // if it is, return the current continuation with the eof object
+    // otherwise, return an eval_continuation with the print continuation
     match e.deref().borrow().deref() {
-        V::EofObject => (k.clone(), A::null()),
+        V::EofObject => (k.clone(), e.clone()),
         _ => {
             let k = A::continuation_plus(print_cont, ports, r, k);
             (A::continuation(eval, r, &k), e.clone())
@@ -46,7 +67,9 @@ fn eval_cont(e: &R, ports: &R, r: &R, k: &R) -> (R, R) {
     }
 }
 
+/// The print continuation function prints the expression evaluated by the eval continuation
+/// It returns a read continuation and a null expression therefore completing the read eval print loop
 fn print_cont(e: &R, ports: &R, r: &R, k: &R) -> (R, R) {
     println!("=> {:?}", e);
-    (A::continuation_plus(prompt_read_cont, ports, r, k), A::null())
+    (A::continuation(read_cont, r, k), ports.clone())
 }
